859749ecbd7e7da8669a7939c93151c987dcb027
VitaGamar <vitaliia.gamar@eleks.com>
2018-02-13 18:13:00 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/index.js',
+    target: 'web',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                include: path.resolve(__dirname, 'src'),
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                include: path.resolve(__dirname, 'src'),
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    },
+    devServer: {
+        port: 3000,
+        contentBase: './dist'
+    }
+};
